795495c72a4b8f6789ccbf2ed8d9777be778a4cf
chandrajeetn <chandrajeetn@lambdatest.com>
2024-06-05 19:25:30 +0530

    more changes

--- examples/key/main.go
+++ examples/key/main.go
@@ -11,7 +11,10 @@
 package main
 
 import (
+	"flag"
 	"fmt"
+	"os"
+	"strings"
 
 	"github.com/go-vgo/robotgo"
 	// "go-vgo/robotgo"
@@ -130,5 +133,18 @@ func key() {
 }
 
 func main() {
-	key()
+	// Define a flag to accept the key combination from the command line
+	keysPtr := flag.String("keys", "", "Key combination to be pressed, e.g., 'cmd+shift+F'")
+	flag.Parse()
+
+	// Check if a key combination was provided
+	if *keysPtr == "" {
+		fmt.Println("Please provide a key combination using the -keys flag.")
+		os.Exit(1)
+	}
+
+	keys := strings.Split(*keysPtr, "^")
+	fmt.Printf("keys: %v\n", strings.Join(keys, "^"))
+	robotgo.KeyTap(keys[len(keys)-1], keys[:len(keys)-1])
+	fmt.Printf("Key combination '%s' pressed.\n", *keysPtr)
 }
